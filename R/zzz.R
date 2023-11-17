cffb6c465bd89a640d9fbfcd96ab9da25ece89ae
Paul Gunsalus <gunsalp@ccf.org>
2023-11-17 14:22:58 -0500

    CRAN polishing

--- R/zzz.R
+++ R/zzz.R
@@ -5,14 +5,13 @@
 }
 .onAttach <- function(libname, pkgname, prompt = NULL) {
   if (!.pkgenv$pres_data) {
-    install_cometdata()
-    # msg <- paste("To use parts of this package, you must install the",
-    #               "cometdata package. To install that",
-    #               "package, run 'remotes::install_github('https://github.com/ClevelandClinicQHS/cometdata', type = 'source')' or press 1."
-    #              # "See the 'COMET' readme for more details."
-    #              )
-    # msg <- paste(strwrap(msg, width = 100), collapse="\n")
-    # cat(msg)
+    msg <- paste("To use parts of this package, you must install the",
+                  "'cometdata' package. To install that",
+                  "package, run 'remotes::install_github('https://github.com/ClevelandClinicQHS/cometdata', type = 'source')'."
+                 # "See the 'COMET' readme for more details."
+                 )
+    msg <- paste(strwrap(msg, width = 100), collapse="\n")
+    cat(msg)
   }
 }
 data_present <- function(pres_data = .pkgenv$pres_data) {
@@ -24,16 +23,17 @@ data_present <- function(pres_data = .pkgenv$pres_data) {
     stop(msg)
   }
 }
-install_cometdata <- function(prompt = NULL){
-  prompt <- if (isTRUE(getOption('knitr.in.progress'))) "n"
-  while (is.null(prompt)) {
-    cat("To use parts of this package, you must install the 'cometdata' package.\nTo install type 1 and press enter or run\n'remotes::install_github('https://github.com/ClevelandClinicQHS/cometdata', type = 'source')'\nPress any other key to continue")
-    prompt <- tolower(readLines(con = stdin(), n = 1L))
-  }
-  if(prompt == 1){
-    cat('cometdata will be installed. You may need to restart R after the installation is complete.')
-    remotes::install_github('https://github.com/ClevelandClinicQHS/cometdata', type = 'source')
-  }else{
-    stop("the package 'cometdata' will not be installed.\nYou will need to install this package to run `run_simulation` and other parts of COMET", call. = FALSE)
-  }
-}
+
+# install_cometdata <- function(prompt = NULL){
+#   prompt <- if (isTRUE(getOption('knitr.in.progress'))) "n"
+#   while (is.null(prompt)) {
+#     cat("To use parts of this package, you must install the 'cometdata' package.\nTo install type 1 and press enter or run\n'remotes::install_github('https://github.com/ClevelandClinicQHS/cometdata', type = 'source')'\nPress any other key to continue")
+#     prompt <- tolower(readLines(con = stdin(), n = 1L))
+#   }
+#   if(prompt == 1){
+#     cat('cometdata will be installed. You may need to restart R after the installation is complete.')
+#     remotes::install_github('https://github.com/ClevelandClinicQHS/cometdata', type = 'source')
+#   }else{
+#     stop("the package 'cometdata' will not be installed.\nYou will need to install this package to run `run_simulation` and other parts of COMET", call. = FALSE)
+#   }
+# }
